a8aec7d554afe6e135ebb47603a58bb0da7b30e7
rg360 <rg360@ymail.com>
2020-10-13 20:40:01 -0500

    first commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            List<string> gamesList = new List<string>()
+            {   
+                "1942(1984)",
+                "Arkanoid(1986)",
+                "Balloon Fight(1984)",
+                "Baraduke(1985)",
+                "Bomb Jack(1984)",
+                "Bubble Bobble(1986)",
+                "BurgerTime(1982)",
+                "Contra(1987)",
+                "Centipede(1981)",
+                "Defender(1981)"
+            };
+
+            //var game = gamesList.Where(x => x.Contains("84"));
+
+            var game = gamesList.OrderBy(x => x.Length);
+
+            foreach (var y in game)
+            {
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
